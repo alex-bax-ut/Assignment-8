2a6c7ef5c14fc69f56b96924b2108922e23b55c1
alexeinbaxley <alex.bax.ut@gmail.com>
2026-04-15 12:03:27 -0600

    Added Menu with Pause and Exit options. Pause will stop the animation and change the button to Start Exit will quit the app.

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -41,6 +41,8 @@ class SpritePreview(QMainWindow):
         # Create needed connections between the UI components and slot methods
         # you define in this class.
         main_layout = QHBoxLayout()
+
+
         self.image = QLabel(self)
         self.image.setPixmap(self.frames[0])
         self.currentFrame = 0
@@ -69,12 +71,14 @@ class SpritePreview(QMainWindow):
         main_layout.addWidget(self.frame_text)
         main_layout.addWidget(self.fps_text)
         main_layout.addWidget(self.start_button)
+        self.add_menu()
 
         frame.setLayout(main_layout)
         self.setCentralWidget(frame)
 
-
-
+    #quits application
+    def quit(self):
+        QApplication.quit()
 
     #updates sprite
     def update(self):
@@ -85,19 +89,42 @@ class SpritePreview(QMainWindow):
 
         self.image.setPixmap(self.frames[self.currentFrame])
 
+    #updates fps based on slider value
     def slider_update(self, value):
         self.fps_text.setText(str(value))
 
+    #stops animation of sprite
+    def stop_frames(self):
+            self.frame_timer.stop()
+            self.start_button.setText("START")
+
+    #starts animation of sprite
     def start_frames(self):
         if self.start_button.text() == "START":
             fps_val = self.slider.value()
             self.frame_timer.start(fps_val)
             self.start_button.setText("STOP")
         else:
-            self.frame_timer.stop()
-            self.start_button.setText("START")
-    # You will need methods in the class to act as slots to connect to signals
+            self.stop_frames()
 
+    #creates menu
+    def add_menu(self):
+        menubar = QMenuBar(None)
+        self.setMenuBar(menubar)
+
+        #add exit action
+        file_menu = menubar.addMenu("&File")
+
+        #add pause option
+        pause_action = QAction('&Pause', self)
+        pause_action.triggered.connect(self.stop_frames)
+        file_menu.addAction(pause_action)
+
+        file_menu.addSeparator()
+
+        exit_action = QAction('&Exit', self)
+        exit_action.triggered.connect(self.quit)
+        file_menu.addAction(exit_action)
 
 def main():
     app = QApplication([])
@@ -106,7 +133,6 @@ def main():
     # And show it
     window.show()
     app.exec()
-
 
 if __name__ == "__main__":
     main()
